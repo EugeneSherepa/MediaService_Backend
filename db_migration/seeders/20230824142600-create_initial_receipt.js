74256e75628d7e5c501b664c16dc492cb8a51010
Eugene Sherepa <evgenijserepa@gmail.com>
2023-08-24 17:33:51 +0300

    migration

--- db_migration/seeders/20230824142600-create_initial_receipt.js
+++ db_migration/seeders/20230824142600-create_initial_receipt.js
@@ -9,6 +9,6 @@ module.exports = {
     }]);
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Users', null, {});
+    return queryInterface.bulkDelete('receipt', null, {});
   }
 };
